d571f914902b79ba5646404a9565fc0cd2f5b4dd
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2021-05-05 12:25:34 +0700

    add UserSelectRoom

---
+++ DibDibDotNet/Controllers/UserSelectRoomController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DibDibDotNet.Models;
+
+namespace DibDibDotNet.Controllers
+{
+    public class UserSelectRoomController : Controller
+    {
+        private readonly ILogger<UserSelectRoomController> _logger;
+
+        public UserSelectRoomController(ILogger<UserSelectRoomController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult UserSelectRoom()
+        {
+            return View();
+        }
+    }
+}

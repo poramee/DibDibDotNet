0a62327081aac26d56fc02590a7a2f503c3886ab
Kittinat A <39628446+porsmart@users.noreply.github.com>
2021-05-08 22:02:45 +0700

    add HomeAdmin

---
+++ DibDibDotNet/Controllers/HomeAdminController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DibDibDotNet.Models;
+
+namespace DibDibDotNet.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        private readonly ILogger<HomeAdminController> _logger;
+
+        public HomeAdminController(ILogger<HomeAdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult HomeAdmin()
+        {
+            return View();
+        }
+    }
+}

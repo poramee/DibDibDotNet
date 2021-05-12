4a5dc00be8438dd558ad8c3efbc7debced6b13ee
Kittinat A <39628446+porsmart@users.noreply.github.com>
2021-05-13 06:25:11 +0700

    Test

--- DibDibDotNet/Controllers/UserSelectRoomController.cs
+++ DibDibDotNet/Controllers/UserSelectRoomController.cs
@@ -18,9 +18,32 @@ namespace DibDibDotNet.Controllers
             _logger = logger;
         }
 
-        public IActionResult UserSelectRoom()
+        public IActionResult UserSelectRoom(string roomId)
         {
-            return View();
+            Room roomModel = new Room();
+            roomModel.RoomNumber = roomId;
+            if (roomId == "ECC-501")
+            {
+                roomModel.EquipmentName = "Xilinx Spartan-6 FPGA";
+                roomModel.PicLocation = "/Assets/ImageRoom501.png";
+            }else if (roomId == "ECC-502")
+            {
+                roomModel.EquipmentName = "YIHUA-948 (2 in 1)";
+                roomModel.PicLocation = "/Assets/ImageRoom502.png";                
+            }else if (roomId == "ECC-504")
+            {
+                roomModel.EquipmentName = "MULTI METER DT1008";
+                roomModel.PicLocation = "/Assets/ImageRoom504.png";                
+            }else if (roomId == "ECC-601")
+            {
+                roomModel.EquipmentName = "STANLEY SCH-12S2-B1";
+                roomModel.PicLocation = "/Assets/ImageRoom601.png";                
+            }else if (roomId == "ECC-602")
+            {
+                roomModel.EquipmentName = "STM32 NUCLEO-F767ZI";
+                roomModel.PicLocation = "/Assets/ImageRoom602.png";                
+            }
+            return View(roomModel);
         }
     }
 }

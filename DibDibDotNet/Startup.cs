0beaa8670a4d777f3b47d796126f82b1246165ef
DrZin-dev <52484219+DrZIn-dev@users.noreply.github.com>
2021-05-24 22:50:44 +0700

    update add cors

--- DibDibDotNet/Startup.cs
+++ DibDibDotNet/Startup.cs
@@ -27,6 +27,8 @@ namespace DibDibDotNet
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+
+      services.AddCors();
       // use session
       services.AddMvc();
       services.AddDistributedMemoryCache();
@@ -50,6 +52,9 @@ namespace DibDibDotNet
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseCors(
+      options => options.WithOrigins("*").AllowAnyMethod()
+      );
       app.UseSession();
       if (env.IsDevelopment())
       {
